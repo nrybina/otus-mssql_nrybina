27f474cf4b3e67d85bbbea288d5cc3e3127da0bd
nrybina <tatarybina@gmail.com>
2025-04-28 22:56:27 +0500

    hw

---
+++ otus-mssql_rybina/HW10_dml/hw_dml_tasks.sql
@@ -0,0 +1,256 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+
+Занятие "10 - Операторы изменения данных".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+1. Довставлять в базу пять записей используя insert в таблицу Customers или Suppliers 
+*/
+declare @id int
+set @id  = (select top 1 CustomerID from sales.Customers order by CustomerID desc)
+--select @id
+
+create table #tmp (id int)
+
+insert into Sales.Customers  ([CustomerID]
+      ,[CustomerName]
+      ,[BillToCustomerID]
+      ,[CustomerCategoryID]
+      ,[BuyingGroupID]
+      ,[PrimaryContactPersonID]
+      ,[AlternateContactPersonID]
+      ,[DeliveryMethodID]
+      ,[DeliveryCityID]
+      ,[PostalCityID]
+      ,[CreditLimit]
+      ,[AccountOpenedDate]
+      ,[StandardDiscountPercentage]
+      ,[IsStatementSent]
+      ,[IsOnCreditHold]
+      ,[PaymentDays]
+      ,[PhoneNumber]
+      ,[FaxNumber]
+      ,[DeliveryRun]
+      ,[RunPosition]
+      ,[WebsiteURL]
+      ,[DeliveryAddressLine1]
+      ,[DeliveryAddressLine2]
+      ,[DeliveryPostalCode]
+      ,[DeliveryLocation]
+      ,[PostalAddressLine1]
+      ,[PostalAddressLine2]
+      ,[PostalPostalCode]
+      ,[LastEditedBy]
+     -- ,[ValidFrom]
+      --,[ValidTo]
+	  )
+	  output inserted.CustomerID  into #tmp
+select TOP (5) --[CustomerID]
+		@id + ROW_NUMBER() over(order by CustomerID)
+      , 'CustomerNameTest' +  cast(ROW_NUMBER() over(order by CustomerID) as nvarchar(1))
+      ,[BillToCustomerID]
+      ,[CustomerCategoryID]
+      ,[BuyingGroupID]
+      ,[PrimaryContactPersonID]
+      ,[AlternateContactPersonID]
+      ,[DeliveryMethodID]
+      ,[DeliveryCityID]
+      ,[PostalCityID]
+      ,[CreditLimit]
+      ,[AccountOpenedDate]
+      ,[StandardDiscountPercentage]
+      ,[IsStatementSent]
+      ,[IsOnCreditHold]
+      ,[PaymentDays]
+      ,[PhoneNumber]
+      ,[FaxNumber]
+      ,[DeliveryRun]
+      ,[RunPosition]
+      ,[WebsiteURL]
+      ,[DeliveryAddressLine1]
+      ,[DeliveryAddressLine2]
+      ,[DeliveryPostalCode]
+      ,[DeliveryLocation]
+      ,[PostalAddressLine1]
+      ,[PostalAddressLine2]
+      ,[PostalPostalCode]
+      ,[LastEditedBy]
+      --,[ValidFrom]
+      --,[ValidTo]  
+	  from Sales.Customers 
+
+	  select * from #tmp
+/*
+2. Удалите одну запись из Customers, которая была вами добавлена
+*/
+delete from Sales.Customers  where CustomerID = (select top 1 id from #tmp)
+
+/*
+3. Изменить одну запись, из добавленных через UPDATE
+*/
+update c set CustomerName = 'CustomerName' from Sales.Customers c where CustomerID  = (select top 1 id from #tmp)
+
+/*
+4. Написать MERGE, который вставит вставит запись в клиенты, если ее там нет, и изменит если она уже есть
+*/
+
+select TOP (1) [CustomerID]
+      ,[CustomerName]
+      ,[BillToCustomerID]
+      ,[CustomerCategoryID]
+      ,[BuyingGroupID]
+      ,[PrimaryContactPersonID]
+      ,[AlternateContactPersonID]
+      ,[DeliveryMethodID]
+      ,[DeliveryCityID]
+      ,[PostalCityID]
+      ,[CreditLimit]
+      ,[AccountOpenedDate]
+      ,[StandardDiscountPercentage]
+      ,[IsStatementSent]
+      ,[IsOnCreditHold]
+      ,[PaymentDays]
+      ,[PhoneNumber]
+      ,[FaxNumber]
+      ,[DeliveryRun]
+      ,[RunPosition]
+      ,[WebsiteURL]
+      ,[DeliveryAddressLine1]
+      ,[DeliveryAddressLine2]
+      ,[DeliveryPostalCode]
+      ,[DeliveryLocation]
+      ,[PostalAddressLine1]
+      ,[PostalAddressLine2]
+      ,[PostalPostalCode]
+      ,[LastEditedBy]
+      --,[ValidFrom]
+      --,[ValidTo]  
+	  into #tmp2
+	  from Sales.Customers 
+
+merge Sales.Customers target
+using #tmp2 source on (target.CustomerID = source.CustomerID)
+when matched 
+	then update set target.CustomerName = 'CustomerName'
+when not matched by target 
+	then insert ([CustomerID]
+      ,[CustomerName]
+      ,[BillToCustomerID]
+      ,[CustomerCategoryID]
+      ,[BuyingGroupID]
+      ,[PrimaryContactPersonID]
+      ,[AlternateContactPersonID]
+      ,[DeliveryMethodID]
+      ,[DeliveryCityID]
+      ,[PostalCityID]
+      ,[CreditLimit]
+      ,[AccountOpenedDate]
+      ,[StandardDiscountPercentage]
+      ,[IsStatementSent]
+      ,[IsOnCreditHold]
+      ,[PaymentDays]
+      ,[PhoneNumber]
+      ,[FaxNumber]
+      ,[DeliveryRun]
+      ,[RunPosition]
+      ,[WebsiteURL]
+      ,[DeliveryAddressLine1]
+      ,[DeliveryAddressLine2]
+      ,[DeliveryPostalCode]
+      ,[DeliveryLocation]
+      ,[PostalAddressLine1]
+      ,[PostalAddressLine2]
+      ,[PostalPostalCode]
+      ,[LastEditedBy]) 
+	  values ([CustomerID]
+      ,[CustomerName]
+      ,[BillToCustomerID]
+      ,[CustomerCategoryID]
+      ,[BuyingGroupID]
+      ,[PrimaryContactPersonID]
+      ,[AlternateContactPersonID]
+      ,[DeliveryMethodID]
+      ,[DeliveryCityID]
+      ,[PostalCityID]
+      ,[CreditLimit]
+      ,[AccountOpenedDate]
+      ,[StandardDiscountPercentage]
+      ,[IsStatementSent]
+      ,[IsOnCreditHold]
+      ,[PaymentDays]
+      ,[PhoneNumber]
+      ,[FaxNumber]
+      ,[DeliveryRun]
+      ,[RunPosition]
+      ,[WebsiteURL]
+      ,[DeliveryAddressLine1]
+      ,[DeliveryAddressLine2]
+      ,[DeliveryPostalCode]
+      ,[DeliveryLocation]
+      ,[PostalAddressLine1]
+      ,[PostalAddressLine2]
+      ,[PostalPostalCode]
+      ,[LastEditedBy]
+	  );
+
+/*
+5. Напишите запрос, который выгрузит данные через bcp out и загрузить через bulk insert
+*/
+
+EXEC sp_configure 'show advanced options', 1;  
+GO  
+-- To update the currently configured value for advanced options.  
+RECONFIGURE;  
+GO  
+-- To enable the feature.  
+EXEC sp_configure 'xp_cmdshell', 1;  
+GO  
+-- To update the currently configured value for this feature.  
+RECONFIGURE;  
+GO  
+
+--SELECT @@SERVERNAME;
+
+DECLARE @out varchar(250);
+set @out = 'bcp WideWorldImporters.Sales.Customers OUT "C:\BCP\test.txt" -T -c -S ' + @@SERVERNAME;
+PRINT @out;
+
+EXEC master..xp_cmdshell @out
+
+DROP TABLE IF EXISTS WideWorldImporters.Sales.Customers_Copy;
+SELECT * INTO WideWorldImporters.Sales.Customers_Copy
+FROM WideWorldImporters.Sales.Customers
+WHERE 1 = 2; 
+
+
+BULK INSERT WideWorldImporters.Sales.Customers_Copy
+    FROM "C:\BCP\test.txt"
+	WITH 
+		(
+		BATCHSIZE = 1000,
+		DATAFILETYPE = 'char',
+		FIELDTERMINATOR = '\t',
+		ROWTERMINATOR ='\n',
+		KEEPNULLS,
+		TABLOCK
+		);
+
+SELECT *
+FROM WideWorldImporters.Sales.Customers_Copy;
